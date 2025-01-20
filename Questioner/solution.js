81b468dbee807a5e2c1c7b88eccaf992a8d6d7d9
MartinL <54684107+marmar333@users.noreply.github.com>
2025-01-20 10:34:41 +0200

    change implemented

--- Questioner/solution.js
+++ Questioner/solution.js
@@ -1,6 +1,17 @@
 changeContent('search-form-content');
 document.querySelector('#search-form-button').addEventListener('click', (e) => searchFormData(e));
 
+let reservation =
+{
+    startDate: null,
+    endDate: null,
+    guestsCount: 0,
+    roomType: null,
+    name: null,
+    phone: null,
+    email: null
+}
+
 function searchFormData(e) {
     e.preventDefault();
     const data = e.target.parentElement;
@@ -15,4 +26,17 @@ function searchFormData(e) {
         console.log(reservation);
         changeContent('search-result-form-content');
     }
+}
+
+function changeContent(className) {
+    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
+    if( document.querySelector(`.${className}`) != null){
+    document.querySelector(`.${className}`).classList.remove('hidden');
+    }
+}
+
+document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
+
+function cleanData(e) {
+    changeContent('search-form-content');
 }
